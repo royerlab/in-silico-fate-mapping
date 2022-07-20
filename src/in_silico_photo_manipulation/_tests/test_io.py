3e3480ff8dcd94c93a8461737fe7be356ef34050
Jordao Bragantini <jordao.bragantini@czbiohub.org>
2022-07-20 14:37:59 -0700

    added writer

--- src/in_silico_photo_manipulation/_tests/test_io.py
+++ src/in_silico_photo_manipulation/_tests/test_io.py
@@ -1,12 +1,18 @@
 from pathlib import Path
+from typing import Callable
 
 import numpy as np
 import pandas as pd
+import pytest
+from napari import Viewer, save_layers
 
 from in_silico_photo_manipulation._reader import napari_get_reader
 
+ViewerMaker = Callable[[], Viewer]
 
-def _build_tracks(n_nodes: int = 10) -> pd.DataFrame:
+
+@pytest.fixture
+def tracks(n_nodes: int = 10) -> pd.DataFrame:
     coordinates = np.random.rand(n_nodes, 3)
     tracks_data = np.zeros((n_nodes, 5))
     tracks_data[:, 2:] = coordinates
@@ -19,9 +25,8 @@ def _build_tracks(n_nodes: int = 10) -> pd.DataFrame:
     return pd.DataFrame(tracks_data, columns=["TrackID", "t", "z", "y", "x"])
 
 
-def test_get_reader(tmp_path: Path):
+def test_get_reader(tmp_path: Path, tracks: pd.DataFrame) -> None:
     path = tmp_path / "good_tracks.csv"
-    tracks = _build_tracks()
     tracks["NodeID"] = np.arange(len(tracks)) + 1
     tracks["Labels"] = np.random.randint(2, size=len(tracks))
     tracks.to_csv(path, index=False)
@@ -39,9 +44,13 @@ def test_get_reader(tmp_path: Path):
     assert np.allclose(data, tracks[["TrackID", "t", "z", "y", "x"]])
 
 
-def test_napari_open(make_napari_viewer, tmp_path: Path):
+def test_napari_read(
+    make_napari_viewer: ViewerMaker,
+    tmp_path: Path,
+    tracks: pd.DataFrame,
+) -> None:
+
     path = tmp_path / "good_tracks.csv"
-    tracks = _build_tracks()
     tracks.to_csv(path, index=False)
 
     viewer = make_napari_viewer()
@@ -51,13 +60,32 @@ def test_napari_open(make_napari_viewer, tmp_path: Path):
     assert np.allclose(layer.data, tracks)
 
 
-def test_non_existing_track():
+def test_napari_write_and_read(
+    make_napari_viewer: ViewerMaker,
+    tmp_path: Path,
+    tracks: pd.DataFrame,
+) -> None:
+
+    viewer = make_napari_viewer()
+    path = tmp_path / "good_tracks.csv"
+
+    layer = viewer.add_tracks(tracks.values)
+
+    (outpath,) = save_layers(path, [layer])
+    assert outpath == path
+
+    viewer.layers.clear()
+    (layer,) = viewer.open(outpath, plugin="in-silico-photo-manipulation")
+
+    assert np.allclose(layer.data, tracks)
+
+
+def test_non_existing_track() -> None:
     reader = napari_get_reader("tracks.csv")
     assert reader is None
 
 
-def test_wrong_columns_track(tmp_path: Path):
-    tracks = _build_tracks()
+def test_wrong_columns_track(tmp_path: Path, tracks: pd.DataFrame) -> None:
     path = tmp_path / "bad_tracks.csv"
     tracks.rename(columns={"TrackID": "id"}, inplace=True)
     tracks.to_csv(path, index=False)
---
+++ src/in_silico_photo_manipulation/_writer.py
@@ -0,0 +1,18 @@
+from typing import List
+
+import numpy as np
+import pandas as pd
+
+from in_silico_photo_manipulation._reader import TRACKS_HEADER
+
+
+def napari_write_tracks(path: str, data: np.ndarray, meta: dict) -> List[str]:
+
+    header = list(TRACKS_HEADER)
+    if data.shape[1] == 4:
+        header.remove("z")
+
+    df = pd.DataFrame(data, columns=header)
+    df.to_csv(path, index=False)
+
+    return [path]
