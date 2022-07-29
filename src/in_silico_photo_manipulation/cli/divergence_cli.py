fixed divergence masking

src/in_silico_photo_manipulation/cli/divergence_cli.py
@@ -55,6 +55,13 @@ def disk(rank: int, radius: int) -> np.ndarray:
     type=click.Path(exists=True, path_type=Path),
     default=None,
 )
+@click.option("--quiet", "-q", type=bool, default=False, is_flag=True)
+@click.option(
+    "--downsample",
+    type=float,
+    default=None,
+    help="Downsample result (CUPY required).",
+)
 def div(
     tracks_path: Path,
     time_point: int,
@@ -63,11 +70,15 @@ def div(
     dilation: int,
     z_scale: float,
     output_path: Optional[Path],
+    quiet: bool,
+    downsample: Optional[float],
 ) -> None:
     """Computes the divergence of tracks from a given time point"""
 
     if output_path is None:
-        output_path = Path(f"output_tp_{time_point}_dilation{dilation}.tif")
+        output_path = Path(
+            f"output_tp_{time_point:05d}_dilation_{dilation}.tif"
+        )
 
     tracks = pd.read_csv(tracks_path)
     tracks["z"] *= z_scale
@@ -92,7 +103,14 @@ def div(
 
     heatmap = divergence(mask, time_point)
 
+    if downsample is not None:
+        import cupy as cp
+        from cupyx.scipy.ndimage import zoom
+
+        heatmap = zoom(cp.asarray(heatmap), downsample).get()
+
     imwrite(output_path, heatmap)
 
-    napari.view_image(heatmap, colormap="magma")
-    napari.run()
+    if not quiet:
+        napari.view_image(heatmap, colormap="magma")
+        napari.run()

src/in_silico_photo_manipulation/divergence.py
@@ -76,28 +76,20 @@ class Divergence(PhotoM):
         _noise = self._get_noise_function(shape)
 
         valid = self._valid_rows(pos)
-        end_points = np.zeros_like(pos)
 
         for t in tqdm(self.time_iter(t0=int(round(t0))), "Computing paths"):
             X = (pos + _noise())[valid]
             if len(X) == 0:
                 break
-            pos[valid] = self._models[t].predict(X)
-            valid = self._valid_rows(X)
-            # points that disappear are added to end points
-            not_valid = np.logical_not(valid)
-            end_points[not_valid] = pos[not_valid]
-        else:
-            # when finished all are added to end points
-            end_points[valid] = pos[valid]
+            next_pos = self._models[t].predict(X)
+            new_valid = self._valid_rows(next_pos)
+            valid[valid] &= new_valid
+            pos[valid] = next_pos[new_valid]
 
-        end_points = end_points.T  # (D, K * N), K = n_samples
-        end_points = end_points.reshape(
-            (shape[1], -1, self.n_samples)
-        )  # (D, N, K)
-        stddev = end_points.std(axis=-1)  # (D, N)
+        pos = pos.T  # (D, K * N), K = n_samples
+        pos = pos.reshape((shape[1], -1, self.n_samples))  # (D, N, K)
+        stddev = pos.std(axis=-1)  # (D, N)
         stddev = stddev.sum(axis=0)  # (N,)
-        stddev = np.nan_to_num(stddev)
 
         divergence = np.zeros(mask.shape, dtype=np.float32)
         divergence[mask] = stddev